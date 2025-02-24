430c5f06911c69cf20e182cadef7d18fb0a00c72
Thiago Scheffer Fialho <thiago.sch.fialho@gmail.com>
2025-02-23 22:44:40 -0300

    fix: Create study values

--- src/routes/study.ts
+++ src/routes/study.ts
@@ -11,10 +11,10 @@ const studyModel = new StudyModel();
 const reviewModel = new ReviewModel();
 
 router.post('/', verifyToken, async function (req: Request, res: Response) {
-  const { topic, qnt_reviews, date, user_id } = req.body;
+  const { topic, qnt_reviews, study_date, user_id } = req.body;
 
   try {
-    if (!topic || !qnt_reviews || !date) {
+    if (!topic || !qnt_reviews || !study_date) {
       return res.status(400).json({ message: "Campos obrigatórios ausentes" });
     }
 
@@ -22,23 +22,24 @@ router.post('/', verifyToken, async function (req: Request, res: Response) {
       return res.status(400).json({ message: "Usuário indefinido" });
     }
 
-    const parsedDate = new Date(date);
+    const parsedDate = new Date(study_date);
     if (isNaN(parsedDate.getTime())) {
       return res.status(400).json({ message: "Formato de data inválido. Use 'yyyy-mm-dd'." });
     }
+    const fixedDate = addDays(parsedDate, 1);
 
-    const formattedDate = format(parsedDate, 'yyyy-MM-dd');
+    const formattedDate = format(fixedDate, 'yyyy-MM-dd');
     const study = await studyModel.createStudy(topic, qnt_reviews, formattedDate, user_id);
 
     let reviews = [];
     for (let i = 0; i < qnt_reviews; i++) {
       let reviewDate;
       if (i === 0) {
-        reviewDate = addDays(parsedDate, 1);
+        reviewDate = addDays(fixedDate, 1);
       } else if (i === 1) {
-        reviewDate = addWeeks(parsedDate, 1);
+        reviewDate = addWeeks(fixedDate, 1);
       } else {
-        reviewDate = addMonths(parsedDate, i - 1);
+        reviewDate = addMonths(fixedDate, i - 1);
       }
 
       const formattedReviewDate = format(reviewDate, 'yyyy-MM-dd');
