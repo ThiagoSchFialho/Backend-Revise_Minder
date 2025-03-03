5d0b8f48ac60d0e9c8c4d39ff81e9587c3969d38
Thiago Scheffer Fialho <thiago.sch.fialho@gmail.com>
2025-03-03 20:41:27 -0300

    fix: Double achievements

--- src/models/achievement.model.ts
+++ src/models/achievement.model.ts
@@ -7,8 +7,17 @@ export class AchievementModel implements IAchievementModel {
   public async createAchievement(name: string, reward: string, user_id: number): Promise<Achievement> {
     const client = await pool.connect();
     let result: QueryResult<Achievement>;
-    
+
     try {
+      const existingAchievement = await client.query(`
+        SELECT * FROM achievements
+        WHERE user_id = $1 AND name = $2;
+      `, [user_id, name]);
+
+      if (existingAchievement.rows.length > 0) {
+        return existingAchievement.rows[0];
+      }
+    
       result = await client.query(`
         INSERT INTO achievements (name, reward, user_id)
         VALUES ($1, $2, $3)
